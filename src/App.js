dc250b5c26f67953f118aa38746d017e2bcb53b8
dariusmaximus <dariulqa@wp.pl>
2020-02-18 20:37:27 +0100

    font-size modified for mobile view, github icon added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import './components/searchForm';
 import SearchForm from './components/searchForm';
+import GithubIcon from '../src/assets/images/icons8-github-60.png';
 
 function App() {
   return (
     <div className="App">
       <SearchForm />
+      <a className="github-btn" target="_blank" rel="noopener noreferrer" href="https://github.com/darius-maximus-girl/TabsFinder"><img src={GithubIcon} alt="github icon"></img></a>
     </div>
   );
 }
