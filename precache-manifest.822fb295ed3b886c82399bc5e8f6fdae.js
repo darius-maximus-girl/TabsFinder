10fff646a2beea50dd90b6b4191ae96832f26a09
dariusmaximus <dariulqa@wp.pl>
2020-02-18 13:44:33 +0100

    Updates

---
+++ precache-manifest.822fb295ed3b886c82399bc5e8f6fdae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff71895b0437df59d09ff1d7ff30473",
+    "url": "/TabsFinder/index.html"
+  },
+  {
+    "revision": "fb7bbf3f77a2df20a53d",
+    "url": "/TabsFinder/static/css/main.f9097f91.chunk.css"
+  },
+  {
+    "revision": "16eb91a4da5a59023ac3",
+    "url": "/TabsFinder/static/js/2.60db6cb3.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/TabsFinder/static/js/2.60db6cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7bbf3f77a2df20a53d",
+    "url": "/TabsFinder/static/js/main.8e27c794.chunk.js"
+  },
+  {
+    "revision": "e10ebde830ccaa2f7bf9",
+    "url": "/TabsFinder/static/js/runtime-main.41c241a7.js"
+  }
+]);
